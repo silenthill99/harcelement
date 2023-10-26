1d9adf374e232cd32d5b9939cde5cd9a2c2137bc
Florian GRAZIANI <florian.graziani@paragon-cc.fr>
2023-10-26 15:57:00 +0200

    Modification du CSS

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ export const Home = () => {
     return(
         <div>
             <div className="flex justify-center">
-                <h1 className="text-3xl font-extrabold m-10">Aide au harcèlement</h1>
+                <h1 className="m-10">Aide au harcèlement</h1>
             </div>
             <p>Bienvenue sur mon site d'aide contre le harcèlement scolaire. Vous pourrez y retrouver différents éléments qui évoquent le sujet, notamment des clips ainsi que des courts métrages.</p>
         </div>
