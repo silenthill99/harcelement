625d361179fab455926adc58ba2af5f641874900
Florian GRAZIANI <florian.graziani@paragon-cc.fr>
2023-10-30 14:12:42 +0100

    Suppression d'éléments dans le .gitignore

--- src/Pages/Bonus.tsx
+++ src/Pages/Bonus.tsx
@@ -9,6 +9,7 @@ export const Bonus = () => {
                 <Videos id="utkuLf8mE6k" name="INFLUENCEUSE - Court Métrage"/>
                 <p>J'aimerais partager avec vous ce petit court métrage qui n'a rien à voir avec le harcèlement,<br/>mais il reste très intéressant car il démontre bien le danger que peuvent représenter les réseaux sociaux</p>
             </div>
+            <p>Essai</p>
         </div>
     )
 }
