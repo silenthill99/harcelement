49e13485b0220651b2e42b35f355a57512389b46
silenthill99 <florian.graziani@sfr.fr>
2023-10-31 19:10:11 +0100

    Suppression de fichiers

--- src/Utils/Videos.tsx
+++ src/Utils/Videos.tsx
@@ -6,7 +6,7 @@ type VideoProps = {
 export const Videos : React.FC<VideoProps> = ({id, name}) => {
     return(
         <div className="flex items-center flex-col">
-            <h2 className="text-2xl font-bold m-1">{name}</h2>
+            <h2 className="m-1">{name}</h2>
             <iframe width="666" height="533" src={"https://www.youtube.com/embed/" + id} title={name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
     )
